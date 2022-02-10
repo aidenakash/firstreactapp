74aaeac52a9d4b20b2a2d7d07c6543d5cf3c0457
aidenakash <96476154+aidenakash@users.noreply.github.com>
2022-02-10 08:18:15 -0500

    commited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import  React from 'react'
 import ReactDOM from 'react-dom'
-ReactDOM.render(<h1>react app</h1>,document.getElementById("root"))
+ReactDOM.render(
+<h1>react app</h1> 
+,document.getElementById("root"))
